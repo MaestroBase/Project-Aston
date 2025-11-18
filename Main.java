a285fc6d10a6e802bc37cb3745ce8c7e4fdbc936
MaestroBase <nikitalobanov.1999@gmail.com>
2025-11-18 22:40:07 +0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,32 @@
+public class Main {
+    public static void main(String[] args) {
+//        System.out.print("1. ");
+//        Lesson_2.printThreeWords();
+//        System.out.print("2. ");
+//        Lesson_2.checkSumSign();
+//        System.out.print("3. ");
+//        Lesson_2.printColor();
+//        System.out.print("4. ");
+//        Lesson_2.compareNumbers();
+//        System.out.print("5. ");
+//        Lesson_2.returnsum();
+//        System.out.print("6. ");
+//        Lesson_2.positive_negative();
+//        System.out.print("7. ");
+//        Lesson_2.positive_negative_TF();
+//        System.out.print("8. ");
+//        Lesson_2.stroka();
+//        System.out.print("9. ");
+//        Lesson_2.Year();
+//        System.out.print("10. ");
+//        Lesson_2.massive();
+//        System.out.print("11. ");
+//        Lesson_2.massive2();
+//        System.out.print("12. ");
+//        Lesson_2.massivelessthan6();
+//        System.out.println("13. ");
+//        Lesson_2.massivewhith1();
+//        System.out.print("14. ");
+//        Lesson_2.SvoiMassive();
+    }
+}
