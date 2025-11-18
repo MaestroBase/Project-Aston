3fbf72a3067ba49760089a69d4b1a76507a34c2d
MaestroBase <nikitalobanov.1999@gmail.com>
2025-11-18 22:49:29 +0400

    Delete Main1.java

--- Main1.java
+++
@@ -1,7 +0,0 @@
-public class Main1 {
-    public static void main(String[] args) {
-        System.out.println("Orange");
-        System.out.println("Banana");
-        System.out.println("Apple");
-    }
-}
